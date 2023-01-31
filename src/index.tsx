919842dd852bd1d1fccc2f3aa90e3559de4fdae2
Schepuk Kamill <kamillschepuk@gmail.com>
2023-01-31 15:11:27 +0300

    refactored root render without const variable

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import AppContainer from './AppContainer';
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<AppContainer />
